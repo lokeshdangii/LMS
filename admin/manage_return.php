759532ddcc3abc1c0e4f9ac03c8ca0899d7ac720
lokeshdangii <lokeshdangi1045@gmail.com>
2023-10-06 14:28:06 +0530

    page to manage book return

--- admin/manage_return.php
+++ admin/manage_return.php
@@ -2,7 +2,7 @@
 	require("functions.php");
 	session_start();
 	#fetch data from database
-	$connection = mysqli_connect("13.51.163.201","phpmyadmin","x8t9aWV3q8Ck");
+	$connection = mysqli_connect("localhost", "root", "");
 	$db = mysqli_select_db($connection,"lmsdb");
 	$name = "";
 	$email = "";
@@ -105,7 +105,7 @@
 						</tr>
 					</thead>
 					<?php
-						$connection = mysqli_connect("13.51.163.201","phpmyadmin","x8t9aWV3q8Ck");
+						$connection = mysqli_connect("localhost", "root", "");
 						$db = mysqli_select_db($connection,"lmsdb");
 						$query = "select * from issued_book";
 						$query_run = mysqli_query($connection,$query);
