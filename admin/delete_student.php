5abb244690b2eb8745dc28d0e710d0da56731561
lokeshdangii <lokeshdangi1045@gmail.com>
2023-10-06 14:15:59 +0530

    logic to delete student

--- admin/delete_student.php
+++ admin/delete_student.php
@@ -1,5 +1,5 @@
 <?php
-	$connection = mysqli_connect("13.51.163.201","phpmyadmin","x8t9aWV3q8Ck");
+	$connection = mysqli_connect("localhost", "root", "");
 	$db = mysqli_select_db($connection,"lmsdb");
 	$query = "delete from student where mobile = $_GET[sid]";
 	$query_run = mysqli_query($connection,$query);
